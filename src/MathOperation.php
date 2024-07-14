cbf6782a5d0304f6397bfe68782e2e4e233086ce
sandhyodip <sandhyodip@samarth.ac.in>
2024-07-14 23:30:56 +0530

    PHP package for arithmetic operation

--- src/MathOperation.php
+++ src/MathOperation.php
@@ -3,10 +3,17 @@ namespace Vendor\ArithmeticOperation;
 
 Class MathOperation {
     const ADDITION='Mathematical operation --> Addition';
+    const SUBSTRACTION='Mathematical operation --> Substraction';
 
     public function addition($a,$b){
         $addition=(int)$a+(int)$b;
         $result= sprintf("Addition of %d and %d is %d.", $a, $b, $addition);
+        return $result;
+    }
+
+    public function substraction($a,$b){
+        $substraction=(int)$a-(int)$b;
+        $result= sprintf("Substraction of %d and %d is %d.", $a, $b, $substraction);
         return $result;
     }
 }
